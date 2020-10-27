b3032588af5f6dc3f60c678827c1eb887a9d3cc0
msamedina <47495545+msamedina@users.noreply.github.com>
2020-10-27 19:03:12 +0100

    Addition of pydeps installs

--- install_additionals.sh
+++ install_additionals.sh
@@ -29,13 +29,20 @@ sudo dpkg -i .dependencies/minisat_2.2.1.deb
 # echo "###Installing pip.."
 # sudo apt-get install python3-pip
 
+# Install python distutils
+# Ubuntu 20.04:
+sudo dpkg -i .dependencies/python3-distutils_3.8.5-1~20.04.1_all.deb
+# Ubuntu 18.04:
+# sudo dpkg -i .dependencies/python3-distutils_3.6.9-1~18.04_all.deb
+
+
 # Install python dependencies
 echo "###Installing python dependencies.."
 pydeppath="$dirpath/.dependencies/pydep"
 # Install pip
 sudo python3 $pydeppath/pip-20.2.4-py2.py3-none-any.whl/pip install --no-index $pydeppath/pip-20.2.4-py2.py3-none-any.whl
 # Install all other python dependencies
-sudo pip install --no-index --find-links $pydeppath -r requirements.txt
+sudo pip install --no-index --find-links $pydeppath -r $pydeppath/requirements.txt
 
 # sudo pip3 install -r requirements.txt
 
