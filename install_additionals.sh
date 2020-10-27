b347fada04b44778b2e91aeb4ef4f2924d843083
msamedina <47495545+msamedina@users.noreply.github.com>
2020-10-27 14:17:52 +0200

    Update install_additionals.sh

--- install_additionals.sh
+++ install_additionals.sh
@@ -13,25 +13,38 @@ preventSubshell(){
 
 preventSubshell
 
+# Unnecessary if all dpendencies are self-contained
 # Update package index
-sudo apt-get update
+# sudo apt-get update
 
+# Get path containing folder
+fullpath=$( realpath "$0" )
+dirpath=$( dirname $fullpath )
+
+#################
 # Install MiniSat
 echo "###Installing MiniSat.."
-sudo apt-get install minisat
+# sudo apt-get install minisat
+sudo dpkg -i .dependencies/minisat_2.2.1.deb
+# minisatpath="$dirpath/.dependencies"
 
+# Unnecessary if all Python dependencies are self-contained
 # Install pip3
-echo "###Installing pip.."
-sudo apt-get install python3-pip
+# echo "###Installing pip.."
+# sudo apt-get install python3-pip
 
 # Install python dependencies
 echo "###Installing python dependencies.."
-sudo pip3 install -r requirements.txt
+pydeppath="$dirpath/.dependencies/pydep"
+# Install pip
+sudo python3 $pydeppath/pip-20.2.4-py2.py3-none-any.whl/pip install --no-index $pydeppath/pip-20.2.4-py2.py3-none-any.whl
+# Install all other python dependencies
+sudo pip install --no-index --find-links $pydeppath -r requirements.txt
+
+# sudo pip3 install -r requirements.txt
 
 # Add NuSMV to PATH variable by adding to .profile
 echo "###Adding NuSMV to PATH.."
-fullpath=$( realpath "$0" )
-dirpath=$( dirname $fullpath )
 nusmvpath="$dirpath/NuSMV-2.6.0-Linux/bin/NuSMV"
 echo "# Allow running NuSMV from terminal" >>~/.profile
 echo "export PATH=\$PATH:$dirpath/NuSMV-2.6.0-Linux/bin" >>~/.profile
