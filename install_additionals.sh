679d11e0671b5d613e10b5c02ae16603ab8620fb
msamedina <47495545+msamedina@users.noreply.github.com>
2020-10-28 07:44:06 +0100

    Resolve dependencies

--- install_additionals.sh
+++ install_additionals.sh
@@ -31,6 +31,7 @@ sudo dpkg -i .dependencies/minisat_2.2.1.deb
 
 # Install python distutils
 # Ubuntu 20.04:
+sudo dpkg -i .dependencies/python3-lib2to3_3.8.2-1ubuntu1_all.deb
 sudo dpkg -i .dependencies/python3-distutils_3.8.5-1~20.04.1_all.deb
 # Ubuntu 18.04:
 # sudo dpkg -i .dependencies/python3-distutils_3.6.9-1~18.04_all.deb
@@ -42,6 +43,7 @@ pydeppath="$dirpath/.dependencies/pydep"
 # Install pip
 sudo python3 $pydeppath/pip-20.2.4-py2.py3-none-any.whl/pip install --no-index $pydeppath/pip-20.2.4-py2.py3-none-any.whl
 # Install all other python dependencies
+sudo pip install --no-index --find-links=$pydeppath testresources
 sudo pip install --no-index --find-links=$pydeppath setuptools
 sudo pip install --no-index --find-links=$pydeppath -r $pydeppath/requirements.txt
 
