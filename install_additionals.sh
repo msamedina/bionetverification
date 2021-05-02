2a4a3f01de5b49aa75389e8b8c5a6c636d0669ba
Avraham Raviv <56107590+AvrahamRaviv@users.noreply.github.com>
2021-05-02 16:17:39 +0300

    Update install_additionals.sh

--- install_additionals.sh
+++ install_additionals.sh
@@ -19,7 +19,7 @@ dirpath=$( dirname $fullpath )
 
 ##################
 # Install all Debian packages:
-sudo dpkg --force-depends -i .dependencies/*.deb
+sudo dpkg -i --force-all .dependencies/*.deb
 # Install MiniSat
 # echo "###Installing MiniSat.."
 # sudo apt-get install minisat
