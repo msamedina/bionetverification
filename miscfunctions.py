5b9174f6f96c943e197c8c03a713137d0ed0e858
msamedina <michelle@aluf-medina.com>
2024-07-04 12:21:52 +0300

    Runtime improvements for GN generation from input file

--- miscfunctions.py
+++ miscfunctions.py
@@ -4,6 +4,8 @@ Michelle Aluf Medina
 """
 import os
 import logging
+import re
+import ast
 from ec import rearrange_universe, bin_rep
 
 
@@ -293,6 +295,8 @@ def read_gn(fn=None):
 	split_top_junc = list()
 
 	specs = list()
+	coord_pattern = re.compile(r'\[x,y\] in (\[\[.*\],*\])')
+	row_pattern = re.compile(r'x in (\[.*\])')
 
 	logging.info('Reading line by line')
 	# Run through the lines of data in the file
@@ -301,46 +305,72 @@ def read_gn(fn=None):
 	logging.info('Save depth.')
 	# Depth - row 0
 	depth = eval((gn[0])[:-1])
+
 	logging.info('Save split rows/coordinates')
 	# Split - row 1
-	sj = lambda x, y: eval(gn[1])
+	split_row = re.search(row_pattern, gn[1])
+	split_coord = re.search(coord_pattern, gn[1])
+
+	# Currently going to assume row length is equal to column length
+	# TODO: Adjust Accordingly for varied input types
+	if split_row:
+		rows = split_row.group(1)
+		for row in rows:
+			for i in range(0, row + 1):
+					split_junc.append([row,i])
+	elif split_coord:
+		coords = split_coord.group(1)
+		split_junc = ast.literal_eval(coords)
+	#sj = lambda x, y: eval(gn[1])
+
 	logging.info('Save reset-false coordinates')
 	# Reset-False - row 2
 	if len(gn) > 2 and gn[2] != '\n':
-		fdj = lambda x, y: eval(gn[2])
-	else:
-		fdj = lambda x, y: False
+		force_down_junc = ast.literal_eval(re.search(coord_pattern, gn[2]).group(1))
+		# fdj = lambda x, y: eval(gn[2])
+	# else:
+		# fdj = lambda x, y: False
+
 	logging.info('Save reset-true coordinates')
 	# Reset-True - row 3
 	if len(gn) > 3 and gn[3] != '\n':
-		rdiagj = lambda x, y: eval(gn[3])
-	else:
-		rdiagj = lambda x, y: False
+		reset_diag_junc = ast.literal_eval(re.search(coord_pattern, gn[3]).group(1))
+		# rdiagj = lambda x, y: eval(gn[3])
+	# else:
+		# rdiagj = lambda x, y: False
+
 	logging.info('Save spit-top coordinates')
 	# Split-Top - row 4
 	if len(gn) > 4 and gn[4] != '\n':
-		s_top = lambda x, y: eval(gn[4])
-	else:
-		s_top = lambda x, y: False
+		split_top_junc = ast.literal_eval(re.search(coord_pattern, gn[4]).group(1))
+		# s_top = lambda x, y: eval(gn[4])
+	# else:
+		# s_top = lambda x, y: False
+
 	logging.info('Save specifications (currently only SMV)')
 	# Specifications - row 5
 	if len(gn) > 5 and gn[5] != '\n':
 		for i in range(5, len(gn)):
 			specs.append(gn[i])
 
-	logging.info('Convert to coordinate lists')
-	for i in range(depth):
-		for j in range(i + 1):
-			if fdj(i, j):
-				force_down_junc.append([i, j])
-			elif rdiagj(i, j):
-				reset_diag_junc.append([i, j])
-			elif s_top(i, j):
-				split_top_junc.append([i, j])
-			elif sj(i, j):
-				split_junc.append([i, j])
+	logging.info('Remove non-split junctions from split list')
+	for r_f in force_down_junc:
+		if r_f in split_junc:
+			split_junc.remove(r_f)
+    		
+	# logging.info('Convert to coordinate lists')
+	# for i in range(depth):
+	# 	for j in range(i + 1):
+	# 		if fdj(i, j):
+	# 			force_down_junc.append([i, j])
+	# 		elif rdiagj(i, j):
+	# 			reset_diag_junc.append([i, j])
+	# 		elif s_top(i, j):
+	# 			split_top_junc.append([i, j])
+	# 		elif sj(i, j):
+	# 			split_junc.append([i, j])
 
-	# read to .txt file
+	# Write to .txt file
 	logging.info('Save General Network')
 	with open('gn.txt', 'w') as f:
 		f.write('General Network with the following architecture:\n')
